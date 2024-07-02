a1f409c22680c025acb90934e4e5fc9489463864
Alexandra Filimonova <al.filimonova@vk.team>
2024-07-02 13:34:30 +0300

    export

--- api/sendForm.ts
+++
@@ -1,8 +0,0 @@
-const baseUrl = "https://api.telegram.org/bot7451642357:AAGiGbgo_pvEga0oawHUYNepKyfzVE5zbLk";
-
-export const sendForm = async (fields: string): Promise<void> => {
-  const url = `${baseUrl}/sendMessage?chat_id=-1002237850047&text=${fields}`;
-
-  const res = await fetch(url);
-  console.log(res);
-};
--- components/sections/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Form } from "./Form/Form";
-export { default as Schedule } from "./Schedule/Schedule";
-export { default as MainPhoto } from "./MainPhoto/MainPhoto";
-export { default as Dresscode } from "./Dresscode/Dresscode";
-export { default as Info } from "./Info/Info";
-export { default as Invite } from "./Invite/Invite";
-export { default as Wishes } from "./Wishes/Wishes";
-export { default as Contacts } from "./Contacts/Contacts";
