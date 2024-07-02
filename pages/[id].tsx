7981755f8facd6a5978047ffbeac7a55397902a1
Alexandra Filimonova <al.filimonova@vk.team>
2024-07-02 21:08:42 +0300

    main page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,4 +1,7 @@
-import { Form } from "@/components/sections";
+import React, { useEffect, useState } from "react";
+
+import { Contacts, Dresscode, Form, Info, Invite, MainPhoto, Schedule, Wishes } from "@/components/sections";
+import { Loader } from "@/components/ui";
 import { guests } from "@/data/guests";
 import { GuestType } from "@/types/guests";
 
@@ -21,11 +24,31 @@ export const getStaticPaths = () => {
   };
 };
 
-const Quiz = ({ params }: StaticParamsType) => {
+const Invitation = ({ params }: StaticParamsType) => {
   const slug = params.id;
   const info: GuestType = guests[slug];
+  const [loading, setLoading] = useState(true);
 
-  return <Form items={info.formItems} slug={slug} />;
+  useEffect(() => {
+    localStorage.setItem("slug", slug);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  });
+
+  return (
+    <div className="main">
+      {loading && <Loader />}
+      <MainPhoto />
+      <Invite title={info.title} text={info.text} />
+      <Info />
+      <Schedule />
+      <Dresscode />
+      <Wishes />
+      <Form items={info.formItems} slug={slug} />
+      <Contacts />
+    </div>
+  );
 };
 
-export default Quiz;
+export default Invitation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui";
 
 const Home = () => {
   const router = useRouter();
-  // const [slug, setSlug] = useState<string | null>(null);
+  const [slug, setSlug] = useState<string | null>(null);
 
   useEffect(() => {
     const s = localStorage.getItem("slug");
-    router.push({ pathname: "/invite", query: { slug: s || "friend" } });
-    // setSlug(s);
+    router.push({ pathname: `/${s}` });
+    setSlug(s);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Link
-        // href={{
-        //   pathname: "/invite",
-        //   query: { slug: slug || "friend" },
-        // }}
-        href="/invite"
-        passHref
-      >
+      <Link href={`/${slug}`} passHref>
         <Button>Открыть приглашение</Button>
       </Link>
     </div>
