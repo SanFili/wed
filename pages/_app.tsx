5ce15bb2e8718f5f1678fcf9bc1dc174b876345d
Alexandra Filimonova <al.filimonova@vk.team>
2024-07-02 17:44:14 +0300

    fonts parhs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import "@/styles/globals.scss";
-import localFont from "next/font/local";
-
-const myFont = localFont({ src: "./Pilowlava-Regular.woff" });
 
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <main className={myFont.className}>
-      <Component {...pageProps} />{" "}
-    </main>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
