1fcc8522077948a140a68575873cd7bf6e14435f
Alexandra Filimonova <al.filimonova@vk.team>
2024-07-04 16:43:58 +0300

    details

--- components/sections/Wishes/Wishes.tsx
+++ components/sections/Wishes/Wishes.tsx
@@ -9,14 +9,24 @@ const Schedule = () => {
       <div className={styles.wishes__flower}>
         <Image src="/images/flower2.svg" alt="flower" title="flower" width="295" height="290" />
       </div>
-      <h2 className={styles.wishes__title}>ПОЖЕЛАНИЯ</h2>
+      <h2 className={styles.wishes__title}>ДЕТАЛИ</h2>
+      <h3>ПОДАРКИ</h3>
       <p className={styles.wishes__text}>
         Лучшие подарки молодой семье — в&#160;конвертах.
-        <br />А еще ваши улыбки&#160;;)
+        <br />А еще ваши улыбки{" "}
+        <span className={styles.wishes__smile}>
+          <Image src="/images/smile.svg" alt="smile" title="smile" width="20" height="20" />
+        </span>
       </p>
+      <h3>ЦВЕТЫ</h3>
       <p className={styles.wishes__text}>
         Пожалуйста, не дарите нам цветы, пусть ваши руки будут свободны для объятий.
       </p>
+      <h3>ПОЖЕЛАНИЯ</h3>
+      {/* <p className={styles.wishes__text}>
+        Наша вечеринка не предполагает присутсвия маленьких детей. Мы будем рады, если в этот день вы сможете отвлечься
+        от родительских забот.
+      </p> */}
       <p className={styles.wishes__text}>Просим вас воздержаться от криков &ldquo;Горько&rdquo; - у нас сладко!</p>
     </section>
   );
